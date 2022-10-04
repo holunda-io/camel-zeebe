d0cdbb86415b72f2ad09879a849a03c8b3918f7c
Jan Galinski <jan.galinski@holisticon.de>
2022-10-04 13:57:37 +0200

    add example app

---
+++ src/main/kotlin/io/holunda/zeebe/camel/component/ZeebeComponent.kt
@@ -0,0 +1,14 @@
+package io.holunda.zeebe.camel.component
+
+import org.apache.camel.Endpoint
+
+class ZeebeComponent {
+
+  companion object {
+    const val SCHEME = "zeebe"
+  }
+
+//  override fun createEndpoint(uri: String, remaining: String?, parameters: MutableMap<String, Any>?): Endpoint {
+//    TODO("Not yet implemented")
+//  }
+}
---
+++ src/test/kotlin/io/holunda/zeebe/camel/_example/CamelConnectorExampleApplication.kt
@@ -0,0 +1,10 @@
+package io.holunda.zeebe.camel.io.holunda.zeebe.camel._example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+fun main() = runApplication<CamelConnectorExampleApplication>().let {  }
+
+@SpringBootApplication
+open class CamelConnectorExampleApplication {
+}
