9623bf52e922651255d5a9a0e936c68cc5ada5d7
Simon Zambrovski <simon.zambrovski@holisticon.de>
2022-10-04 14:44:38 +0200

    implementing worker

--- src/main/kotlin/io/holunda/zeebe/camel/connector/CamelWorker.kt
+++ src/main/kotlin/io/holunda/zeebe/camel/connector/CamelWorker.kt
@@ -3,20 +3,23 @@ package io.holunda.zeebe.camel.connector
 import io.camunda.zeebe.client.api.response.ActivatedJob
 import io.camunda.zeebe.client.api.worker.JobClient
 import io.camunda.zeebe.spring.client.EnableZeebeClient
+import io.camunda.zeebe.spring.client.annotation.ZeebeVariable
 import io.camunda.zeebe.spring.client.annotation.ZeebeWorker
 import mu.KLogging
 
 @EnableZeebeClient
 class CamelWorker {
 
-  companion object: KLogging()
+  companion object : KLogging()
 
-  @ZeebeWorker(type = "io.holunda:camel:1")
-  fun camel(client: JobClient, job: ActivatedJob) {
-    logger.info { "Calling camel client" }
+  @ZeebeWorker(type = "io.holunda:camel:1", autoComplete = true)
+  fun camel(client: JobClient, job: ActivatedJob, @ZeebeVariable endpoint: String) {
+    logger.info { "Calling camel client for endpoint $endpoint" }
 
     val variables = job.variablesAsMap
-    logger.trace { "Variables: $variables" }
+
+    logger.info { "Endpoint: $endpoint Variables: $variables" }
+
     client.newCompleteCommand(job.key)
       .send()
       .exceptionally { throwable -> throw RuntimeException("Could not complete job $job", throwable) }
