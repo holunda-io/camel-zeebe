d751d63112c1fb465713cad76ef2d9ef6a073ea0
Jan Galinski <jan.galinski@holisticon.de>
2022-10-04 15:40:48 +0200

    wip

--- src/main/kotlin/io/holunda/zeebe/camel/component/DynamicCamelTaskWorker.kt
+++ src/main/kotlin/io/holunda/zeebe/camel/component/DynamicCamelTaskWorker.kt
@@ -1,9 +1,28 @@
 package io.holunda.zeebe.camel.component
 
 import org.apache.camel.CamelContext
+import org.apache.camel.builder.RouteBuilder
+import org.springframework.stereotype.Component
 
-
+@Component
 class DynamicCamelTaskWorker(
-  val camelContext: CamelContext
-) {
+  camelContext: CamelContext
+): (String, Map<String,Any>) -> Unit, RouteBuilder(camelContext) {
+  companion object {
+    const val START = "direct:dynamicCamelTaskWorker"
+    const val KEY_ENDPOINT = "zeebeEndpoint"
+  }
+
+  override fun invoke(endPoint: String, body: Map<String, Any>) {
+    super.getCamelContext().createFluentProducerTemplate()
+      .withHeader(KEY_ENDPOINT, endPoint)
+      .withBody(body)
+      .to(START)
+      .request()
+  }
+
+  override fun configure() {
+    from(START)
+      .toD("\${header.$KEY_ENDPOINT}")
+  }
 }
